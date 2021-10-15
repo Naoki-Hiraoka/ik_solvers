a7b2cfe0a7e39041f07a8a90cadc12358e66ec7a
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2021-10-15 14:42:19 +0900

    fix

--- ik_constraint_joint_limit_table/include/ik_constraint_joint_limit_table/JointLimitMinMaxTableConstraint.h
+++ ik_constraint_joint_limit_table/include/ik_constraint_joint_limit_table/JointLimitMinMaxTableConstraint.h
@@ -8,20 +8,9 @@ namespace ik_constraint_joint_limit_table{
   class JointLimitMinMaxTableConstraint : public IK::JointLimitConstraint {
   public:
     //jointのqをq_upperとq_lowerの間かつmin-max-tableの間にさせる.
-    //  maxError: エラーの頭打ち
-    //  precision: 収束判定の閾値
-    //  weight: コスト関数の重み. error * weight^2 * error.
 
-    const cnoid::LinkPtr& joint() const { return joint_;}
-    cnoid::LinkPtr& joint() { return joint_;}
     const std::vector<std::shared_ptr<joint_limit_table::JointLimitTable> >& jointLimitTables() const { return jointLimitTables_;}
     std::vector<std::shared_ptr<joint_limit_table::JointLimitTable> >& jointLimitTables() { return jointLimitTables_;}
-    const double& maxError() const { return maxError_;}
-    double& maxError() { return maxError_;}
-    const double& precision() const { return precision_;}
-    double& precision() { return precision_;}
-    const double& weight() const { return weight_;}
-    double& weight() { return weight_;}
 
     virtual bool checkConvergence () override;
 
